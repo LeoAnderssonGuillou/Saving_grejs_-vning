e3bc7452944b30fca05d056fb564f63911e73c7e
Leo <leo.anderssonguillou@elev.ga.ntig.se>
2021-10-11 12:52:54 +0200

    First

---
+++ Saving_grejs_övning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Saving_grejs_övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
